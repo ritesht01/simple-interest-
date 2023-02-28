4337f96ebc01927211b7cb146d952e6bfba3c718
ritesht01 <125853640+ritesht01@users.noreply.github.com>
2023-02-28 10:55:50 +0545

    add printf and scanf

--- simple interest.c
+++ simple interest.c
@@ -2,7 +2,8 @@
 int main()
 {
 float P , R , T , SI ;
-P =34000; R =30; T = 5;
+printF("enter the principal, rate amd time:");
+scanf("%d,%f,%f", &P ,&R ,&T);
 SI = (P*R*T)/100;
 printf("\n\n Simple Interest is : %f", SI);
 return (0);
